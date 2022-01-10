bb2260d20d306712d3d318d3150a4c6933f172f7
TDCIAN <tdcian71@gmail.com>
2022-01-11 00:39:12 +0900

    add locationdata, kldocument, detaillistcelldata

---
+++ FindCVS/Entities/DetailListCellData.swift
@@ -0,0 +1,15 @@
+//
+//  DetailListCellData.swift
+//  FindCVS
+//
+//  Created by JeongminKim on 2022/01/11.
+//
+
+import Foundation
+
+struct DetailListCellData {
+    let placeName: String
+    let address: String
+    let distance: String
+    let point: MTMapPoint
+}
---
+++ FindCVS/Entities/LocationData.swift
@@ -0,0 +1,14 @@
+//
+//  LocationData.swift
+//  FindCVS
+//
+//  Created by JeongminKim on 2022/01/11.
+//
+
+import Foundation
+
+struct LocationData: Decodable {
+    let documents: [KLDocument]
+}
+
+
